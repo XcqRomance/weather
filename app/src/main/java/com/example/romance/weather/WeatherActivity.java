90af83e94a161f2d353af0cc83e20c48dc14904b
XcqRomance <755412806@qq.com>
2017-12-13 19:27:37 +0800

    天气后台自动更新

--- app/src/main/java/com/example/romance/weather/WeatherActivity.java
+++ app/src/main/java/com/example/romance/weather/WeatherActivity.java
@@ -1,15 +1,19 @@
  package com.example.romance.weather;
 
+ import android.content.Intent;
  import android.content.SharedPreferences;
  import android.graphics.Color;
  import android.os.Build;
  import android.os.Bundle;
  import android.preference.PreferenceManager;
+ import android.support.v4.view.GravityCompat;
+ import android.support.v4.widget.DrawerLayout;
  import android.support.v4.widget.SwipeRefreshLayout;
  import android.support.v7.app.AppCompatActivity;
  import android.util.Log;
  import android.view.LayoutInflater;
  import android.view.View;
+ import android.widget.Button;
  import android.widget.ImageView;
  import android.widget.LinearLayout;
  import android.widget.ScrollView;
@@ -18,6 +22,7 @@
 
  import com.bumptech.glide.Glide;
  import com.example.romance.weather.gson.Weather;
+ import com.example.romance.weather.service.AutoUpdateService;
  import com.example.romance.weather.util.HttpUtil;
  import com.example.romance.weather.util.Utility;
  import com.google.gson.annotations.SerializedName;
@@ -45,6 +50,8 @@
     private ImageView bingImg;
     public SwipeRefreshLayout swipeRefresh;
     private String mWeatherId;
+    public DrawerLayout drawerLayout;
+    private Button navButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +78,8 @@
         carWashText = (TextView)findViewById(R.id.car_wash_text);
         sportText = (TextView)findViewById(R.id.sport_text);
         bingImg = (ImageView)findViewById(R.id.bing_big_img);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        navButton = (Button)findViewById(R.id.nav_button);
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather",null);
@@ -97,6 +106,13 @@
         } else {
             loadBingPic();
         }
+
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 
     private void loadBingPic() {
@@ -123,7 +139,7 @@
     }
 
     /*根据id，请求城市的天气*/
-    private void requestWeather(final String weatherId) {
+    public void requestWeather(final String weatherId) {
         String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=41dd96cb90344217acbf5fe0813f16cd";
         Log.i("weatherUrl",weatherUrl);
         HttpUtil.sendOkhttpRequest(weatherUrl, new Callback() {
@@ -194,6 +210,8 @@
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
 
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
  }
